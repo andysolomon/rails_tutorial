b3532b87f0fd32b8c04da0bdecc9180b4c3eda38
Ubuntu <ubuntu@domU-12-31-39-10-82-7A.compute-1.internal>
2012-02-10 21:22:47 +0000

    wrapping up chapter 4

---
+++ example_user.rb
@@ -0,0 +1,11 @@
+class User
+  attr_accessor :name, :email
+
+  def initilize attributes = {}
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+end
