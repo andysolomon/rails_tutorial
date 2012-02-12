85757ca2fe5fcc501e825c90daeb5b914b89b393
Ubuntu <ubuntu@domU-12-31-39-10-82-7A.compute-1.internal>
2012-02-12 23:06:23 +0000

    Remove default Rails page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,11 +7,11 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => "Sample App")
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', 
 			:text => "#{base_title} | Home")
     end
@@ -20,11 +20,11 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => "Help")
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', 
 			:text => "#{base_title} | Help")
     end
@@ -33,11 +33,11 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', 
 			:text => "#{base_title} | About")
     end
@@ -46,11 +46,11 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact Us')
     end
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', 
 			:text => "#{base_title} | Contact")
     end
