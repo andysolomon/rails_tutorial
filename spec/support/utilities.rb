b28b47aa4ee9e3323847da619b854ea5e64a10d7
Ubuntu <ubuntu@domU-12-31-39-10-82-7A.compute-1.internal>
2012-03-20 16:51:47 +0000

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,4 @@
-def full_title(page_title)
+def full_title page_title
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
     base_title
@@ -7,3 +7,11 @@ def full_title(page_title)
   end
 end
 
+def sign_in user
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
